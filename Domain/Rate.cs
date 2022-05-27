3cb01bb561987b0652f20f6baa193eb3435d7303
Roei-Cohen <76795177+roei-stack@users.noreply.github.com>
2022-05-27 19:27:21 +0300

    Add files via upload

---
+++ Domain/Rate.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BorisWeb.Models
+{
+    public class Rate
+    {
+        [Key]
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int Rating { get; set; } = 1;
+
+        [MaxLength(100)]
+        public string Feedback { get; set; }
+
+        public DateTime date { get; set;}
+    }
+}
---
+++ Services/RateService.cs
@@ -0,0 +1,40 @@
+﻿
+using BorisWeb.Models;
+
+namespace BorisWeb.Services 
+{
+    public class RateService 
+    {
+
+        private static List<Rate> rates = new List<Rate>();
+        public List<Rate> GetAll() {
+            return rates;
+        }
+
+        public Rate Get(string username) {
+            return rates.Find(x => x.Username == username);
+        }
+
+        public void Create(string username, int rating, string feedback) {
+            if (Get(username) != null) {
+                return;
+            }
+            rates.Add(new Rate() { Username = username, Rating = rating, Feedback = feedback });
+        }
+
+        public bool IsEmpty()
+        {
+            return rates.Count == 0;
+        }
+
+        public void Edit(string username, int rating, string feedback) {
+            Rate rate = Get(username);
+            rate.Rating = rating;
+            rate.Feedback = feedback;
+        }
+
+        public void Delete(string username) {
+            rates.Remove(Get(username));
+        }
+    }
+}
